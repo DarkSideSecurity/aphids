232b24af85740af9916b47a3eded6edbdfd8d371
0xGh0st <n0man@protonmail.com>
2026-01-29 10:45:01 -0500

    version update.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='aphids-cli',
-    version='1.2.5',
+    version='1.2.6',
     author='Evan Magrann',
     author_email='admin@darksidesecurity.io',
     packages=find_packages()+['.'],
